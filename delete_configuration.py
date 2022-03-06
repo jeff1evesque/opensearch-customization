15a06a0823187ee7a2613f08f44d644fb2168c9e
Jeff Levesque <jeff1evesque@yahoo.com>
2022-03-06 09:57:05 -0500

    "#26: define initial 'delete_document'"

--- delete_configuration.py
+++ delete_configuration.py
@@ -36,3 +36,59 @@ def delete_index(
         return False
 
     return False
+
+
+def delete_document(
+    endpoint,
+    awsauth,
+    index_name,
+    index_range,
+    headers=json.loads(os.getenv('Headers', '{"Content-Type": "application/json"}').strip())
+):
+    '''
+
+    delete index documents satisfying specified range
+
+    @range, object with the following structure
+
+        {
+            "timestamp": {
+                "lte": "now-5d"
+            }
+        }
+
+    '''
+
+    if index_name and index_range:
+        path = '{}/_delete_by_query'.format(index_name)
+        payload = { 'query': { 'range': index_range } }
+
+    else:
+        print('Error (delete_document): path and payload not configured')
+        return False
+
+    try:
+        r = requests.post(
+            '{}/{}'.format(endpoint, path),
+            auth=awsauth,
+            json=payload,
+            headers=headers
+        )
+
+        if r.ok:
+            print('Notice: documents in {} deleted satisfying {}'.format(
+                index_name,
+                index_range
+            ))
+            return True
+
+        print('Notice (delete_document): for {} returned {}'.format(
+            index_name,
+            r.status_code
+        ))
+
+    except Exception as e:
+        print('Error (delete_document): {}'.format(e))
+        return False
+
+    return False
